8637240ff382762f59da8ab73c59c2117a53a02b
Hami483 <mzmohamedarif@gmail.com>
2022-04-04 15:27:20 +0530

    fixed code smell1

--- app/api/episodes.js
+++ app/api/episodes.js
@@ -1,29 +1,29 @@
 export async function getAllEpisodes(){
     const response = await fetch(`https://www.breakingbadapi.com/api/episodes?series=Breaking+Bad`)
 
-    return await response.json()
+    return response.json()
 }
 
 export async function getEpisodeById(id){
     const response = await fetch(`https://www.breakingbadapi.com/api/episodes/${id}`)
 
-    return await response.json()
+    return response.json()
 }
 
 export async function getCharacterByName(name){
     const response = await fetch(`https://www.breakingbadapi.com/api/characters?name=${name}`)
 
-    return await response.json()
+    return response.json()
 }
 
 export async function getAllCharacters(name=''){
     const response = await fetch(name ? `https://www.breakingbadapi.com/api/characters?name=${name}`  :`https://www.breakingbadapi.com/api/characters`)
 
-    return await response.json()
+    return response.json()
 }
 
 export async function getCharacterById(id){
     const response = await fetch(`https://www.breakingbadapi.com/api/characters/${id}`)
 
-   return await response.json()
+   return response.json()
 }
